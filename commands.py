787e3ec6b24a07b432feebdce9fb70d29844855f
Lucas Denis <lucas.denis@etu.uca.fr>
2023-11-12 11:29:01 +0100

    Test for Sylvanas

--- commands.py
+++ commands.py
@@ -2,11 +2,25 @@ import discord
 
 def init_bot_commands(bot):
 
-    class MyView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
+    class TestView(discord.ui.View): # Create a class called MyView that subclasses discord.ui.View
         @discord.ui.button(label="Click me!", style=discord.ButtonStyle.success, emoji="😎") # Create a button with the label "😎 Click me!" with color Blurple
         async def button_callback(self, button, interaction):
             await interaction.response.send_message("You clicked the button!") # Send a message when the button is clicked
 
     @bot.command(description="Test button") # Create a slash command
     async def button(ctx):
-        await ctx.respond("This is a button!", view=MyView()) # Send a message with our View class that contains the button
+        await ctx.respond("This is a button!", view=TestView()) # Send a message with our View class that contains the button
+
+    @bot.command(description="Create Sylvanas")
+    async def sylvanas(ctx):
+        embed = discord.Embed(
+            title="Sylvanas Windrunner",
+            description="The Banshee Queen",
+            color=discord.Colour.orange(), # Pycord provides a class with default colors you can choose from
+        )
+        embed.set_author(name="Forsaken", icon_url="https://static.wikia.nocookie.net/wowpedia/images/7/72/Forsaken_Crest.png/revision/latest?cb=20151113054325")
+        embed.set_image(url="https://static.wikia.nocookie.net/wow/images/e/e2/Sylvanas_JCC.jpg/revision/latest/scale-to-width-down/392?cb=20230811211943&path-prefix=fr")
+        embed.set_footer(text='"We are the Forsaken. We will slaughter anyone who stands in our way."')
+
+        await ctx.respond(embed=embed) # Send the embed with some text
+ 
