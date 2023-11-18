2f320f7d10dee94c828827f2e22c5f9cbbbf58d7
Lucas Denis <lucas.denis@etu.uca.fr>
2023-11-18 11:33:10 +0100

    Initial shop logic

--- database.py
+++ database.py
@@ -323,11 +323,11 @@ def calculate_dust_earned(rarity: str) -> int:
         int: The amount of dust earned.
     """
     dust_values = {
-        'legendary': 500,  # TODO: Update with actual value
-        'epic': 200,       # TODO: Update with actual value
-        'rare': 100,       # TODO: Update with actual value
-        'uncommon': 50,   # TODO: Update with actual value
-        'common': 10,     # TODO: Update with actual value
+        'legendary': 500,  
+        'epic': 200,       
+        'rare': 100,       
+        'uncommon': 50,   
+        'common': 10,     
     }
     return dust_values.get(rarity.lower(), 0)  # Default to 0 if rarity not found
 
@@ -387,6 +387,108 @@ def get_dust_balance(user_id: str, server_id: str) -> int:
     finally:
         conn.close()
 
+
+def craft_card(user_id: str, card_id: int, server_id: int, cost: int) -> bool:
+    """
+    Handles the crafting of a card for a user.
+
+    Args:
+        user_id (str): The user's ID.
+        card_id (int): The ID of the card being crafted.
+        server_id (int): The ID of the server where the card is being crafted.
+        cost (int): The dust cost of the card.
+
+    Returns:
+        bool: True if crafting was successful, False otherwise.
+    """
+    conn = sqlite3.connect("database.sqlite")
+    cur = conn.cursor()
+
+    try:
+
+        # Check if user exists
+        cur.execute("SELECT id FROM User WHERE userID = ? AND serverId = ?", (user_id, server_id))
+        user_row = cur.fetchone()
+
+        if user_row is None:
+            # Create a new user if not exists
+            cur.execute("INSERT INTO User (userID, serverID) VALUES (?, ?)", (user_id, server_id))
+            user_db_id = cur.lastrowid
+        else:
+            user_db_id = user_row[0]
+
+        # Check and update the user's dust balance
+        cur.execute("SELECT balance FROM DustBalance WHERE userID = ?", (user_id,))
+        result = cur.fetchone()
+        if result and result[0] >= cost:
+            new_balance = result[0] - cost
+            cur.execute("UPDATE DustBalance SET balance = ? WHERE userID = ?", (new_balance, user_id))
+        else:
+            return False  # Not enough dust
+
+
+        # Add the card to the user's collection
+        cur.execute("INSERT INTO UserCard (userID, cardID) VALUES (?, ?)", (user_db_id, card_id))
+        conn.commit()
+        return True
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+        return False
+    finally:
+        conn.close()
+
+
+def get_shop_inventory() -> list:
+    """
+    Retrieves the current inventory of cards in the shop.
+
+    Returns:
+        list: A list of tuples representing the cards in the shop.
+    """
+    conn = sqlite3.connect("database.sqlite")
+    cur = conn.cursor()
+
+    try:
+        cur.execute("""
+        SELECT c.id, c.name, co.name, c.title, c.quote, c.imageURL, c.rarity, (CASE c.rarity WHEN 'legendary' THEN 1000 WHEN 'epic' THEN 400 WHEN 'rare' THEN 200 WHEN 'uncommon' THEN 100 ELSE 50 END) as cost
+        FROM Card c
+        JOIN Collection co ON c.collectionID = co.id
+        ORDER BY RANDOM()
+        LIMIT 3;
+        """)
+        cards = cur.fetchall()
+        return cards
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+        return []
+    finally:
+        conn.close()
+
+def check_user_dust_balance(user_id: str, cost: int) -> bool:
+    """
+    Checks if the user has enough dust to craft a card.
+
+    Args:
+        user_id (str): The user's ID.
+        cost (int): The dust cost of the card.
+
+    Returns:
+        bool: True if the user has enough dust, False otherwise.
+    """
+    conn = sqlite3.connect("database.sqlite")
+    cur = conn.cursor()
+
+    try:
+        cur.execute("SELECT balance FROM DustBalance WHERE userID = ?", (user_id,))
+        result = cur.fetchone()
+        if result:
+            return result[0] >= cost
+        return False
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+        return False
+    finally:
+        conn.close()
 
 
 ## HACK: This is a temporary function to reset the cooldown for the user
