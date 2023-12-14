3066c1cab80a759f39a3228bf0ebd0db1ca412ab
ElRapt <lucas.denis@etu.uca.fr>
2023-12-14 14:08:49 +0100

    Order fixes

--- database/cards.py
+++ database/cards.py
@@ -31,17 +31,6 @@ def get_random_card() -> Optional[Tuple[int, str, str, str, str, str, str]]:
     return card if card else None
 
 def get_user_collection(user_id: str, server_id: int) -> Optional[list]:
-    """
-    Fetches the user's collection in a specific server.
-
-    Args:
-        user_id (str): The user's Discord ID.
-        server_id (int): The Discord server's ID where the collection is being fetched.
-
-    Returns:
-        A list of tuples containing the card's name, collection name, title, quote, imageURL, and rarity if found.
-        None if no cards are available.
-    """
     conn = sqlite3.connect("database.sqlite")
     cur = conn.cursor()
     
@@ -50,7 +39,14 @@ def get_user_collection(user_id: str, server_id: int) -> Optional[list]:
         SELECT ca.name, co.name, ca.title, ca.quote, ca.imageURL, ca.rarity FROM UserCard uc
         JOIN Card ca ON uc.cardID = ca.id
         JOIN Collection co ON ca.collectionID = co.id
-        WHERE uc.userID = ? AND uc.serverID = ?;
+        WHERE uc.userID = ? AND uc.serverID = ?
+        ORDER BY CASE ca.rarity
+            WHEN 'legendary' THEN 1
+            WHEN 'epic' THEN 2
+            WHEN 'rare' THEN 3
+            WHEN 'uncommon' THEN 4
+            WHEN 'common' THEN 5
+        END, ca.name;
         """, (user_id, server_id))
         cards = cur.fetchall()
     except sqlite3.Error as e:
@@ -78,7 +74,6 @@ def get_collections() -> List[str]:
         conn.close()
         
 def fetch_cards_by_collection(user_id: str, collection_name: str) -> List[Tuple]:
-    """ Fetch cards for a specific user filtered by collection name. """
     conn = sqlite3.connect("database.sqlite")
     try:
         with conn:
@@ -89,6 +84,13 @@ def fetch_cards_by_collection(user_id: str, collection_name: str) -> List[Tuple]
             JOIN Collection co ON c.collectionID = co.id
             JOIN UserCard uc ON c.id = uc.cardID
             WHERE uc.userID = ? AND LOWER(co.name) = LOWER(?)
+            ORDER BY CASE c.rarity
+                WHEN 'legendary' THEN 1
+                WHEN 'epic' THEN 2
+                WHEN 'rare' THEN 3
+                WHEN 'uncommon' THEN 4
+                WHEN 'common' THEN 5
+            END, c.name;
             """, (user_id, collection_name))
             cards = cur.fetchall()
             return cards
